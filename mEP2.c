791d245be81cf1177c83ec6928009ea32f24536b
Victor Trintim <victortrintim@gmail.com>
2021-07-07 23:27:43 -0300

    mEP2

---
+++ mEP2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    long int *p;
+    long int **B;
+    int i, j;
+    long int n;
+    int pos = 1;
+
+    scanf("%ld", &n);
+
+    //p vetor de permutacao
+    p = malloc(n*sizeof(long int));
+    if (p == NULL){
+        exit(1);
+    }
+
+    B = malloc(n*sizeof(long int*));
+    if (B == NULL){
+        exit(1);
+    }
+
+    for(i = 0; i < n; i++){
+        B[i] = malloc(n*sizeof(long int));
+        if (B[i] == NULL){
+            exit(1);
+        }
+    }
+
+    for(i = 0; i < n; i++){
+        scanf("%ld", &p[i]);
+    }
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            scanf("%ld", &B[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++){
+        if(p[i] == pos && pos <= n){
+            for(j = 0; j < n; j++){
+                printf("%ld ", B[i][j]);
+            }
+            printf("\n");
+            pos++;
+            i=-1;
+        }
+    }
+
+    for (i = 0; i < n; i++){
+        free(B[i]);
+    }
+    free(B);
+    free(p);
+
+    return 0;
+}
